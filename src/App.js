eff74f9b940b5806e9c57fa8716dac5b3e5a5b13
Albangh <90769567+Albangh@users.noreply.github.com>
2022-05-26 21:05:42 +0200

    deploy v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { isEmpty } from "./utils/utils";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState();
+  const [index, setIndex] = useState(1);
+
+  const generate = () => {
+    const index = Math.floor(Math.random() * data.length);
+    setIndex(index);
+  };
+
+  useEffect(() => {
+    function fetchData() {
+      axios({
+        method: "get",
+        url: "http://localhost:5000/api",
+      })
+        .then((res) => setData(res.data))
+        .catch((err) => console.log(err));
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="desktop">
+      <div className="header">
+        <img src="./images/icon.svg" alt="logo" />
+        <h1>
+          Le <br /> Penseur
+        </h1>
+      </div>
+      {!isEmpty(data) && (
+        <div className="card">
+          <img className="paperclip" src="./images/paperclip.png" alt="paperclip" />
+          <div className={data[index].category === "Géographie" ? "geography" : "category" && data[index].category === "Animaux" ? "animal" : "category" && data[index].category === "Histoire" ? "history" : "category" && data[index].category === "Art et Culture" ? "art" : "category"  && data[index].category === "Sport" ? "sport" : "category" && data[index].category === "Histoire" ? "history" : "category" && data[index].category === "Art et Culture" ? "art" : "category"  && data[index].category === "Insolite" ? "funny" : "category" && data[index].category === "Nourriture" ? "food" : "category"} >
+            <span>{data[index] && data[index].category}</span>
+          </div>
+
+          <p>{data[index] && data[index].content}</p>
+          <button className="btn-push" onClick={generate}><img src="./images/arrow.svg" alt="arrow" /></button>
+        </div>
+      )}
+
+      <div className="footer">
+        <p>2022 - alban</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
